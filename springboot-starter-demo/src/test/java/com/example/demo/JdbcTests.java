42a104351876d9f4131a4e9ae68b2443e5ec4f79
qingmei <qingmei@fanhaoyue.com>
2020-04-18 16:44:40 +0800

    更改命名

--- springboot-starter-demo/src/test/java/com/example/demo/JdbcTests.java
+++ springboot-starter-demo/src/test/java/com/example/demo/JdbcTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
-class SpringbootStarterDemoApplicationTests {
+class JdbcTests {
 
     @Autowired
     JdbcTemplate db2JdbcTemplate;
