affff5e0ad95bbdd03ec9a52d6cca2941f9bda18
qingmei <qingmei@fanhaoyue.com>
2020-04-18 13:26:54 +0800

    first

---
+++ format-spring-boot-starter/src/main/java/com/example/starter/autoconfiguration/FormatAutoConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.starter.autoconfiguration;
+
+
+import com.example.starter.format.FormatProcessor;
+import com.example.starter.format.JsonFormatProcessor;
+import com.example.starter.format.StringFormatProcessor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class FormatAutoConfiguration {
+
+    @ConditionalOnMissingClass("com.alibaba.fastjson.JSON")
+    @Bean
+    @Primary
+    public FormatProcessor stringFormat(){
+        return new StringFormatProcessor();
+    }
+
+    @ConditionalOnClass(name = "com.alibaba.fastjson.JSON")
+    @Bean
+    public FormatProcessor jsonFormat(){
+        return new JsonFormatProcessor();
+    }
+
+}
---
+++ format-spring-boot-starter/src/main/java/com/example/starter/autoconfiguration/HelloAutoConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.starter.autoconfiguration;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.example.starter.HelloFormatTemplate;
+import com.example.starter.format.FormatProcessor;
+
+@Import(FormatAutoConfiguration.class)
+@EnableConfigurationProperties(HelloProperties.class)
+@Configuration
+public class HelloAutoConfiguration {
+
+    @Bean
+    public HelloFormatTemplate helloFormatTemplate(HelloProperties helloProperties, FormatProcessor formatProcessor) {
+        return new HelloFormatTemplate(helloProperties, formatProcessor);
+    }
+}
